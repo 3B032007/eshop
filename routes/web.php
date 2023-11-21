281ad0431e83b26e5b12cfcdaede7887a7e3df8b
劉秉樺 <alen920617@gmail.com>
2023-11-21 22:20:47 +0800

    ，回答每一個路由名稱所用 的 URL、HTTP 方法(或稱 HTTP 動詞)、與所串接的控制器&方法

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Products', ProductController::class);
+
+//product.index URL:/Products
+//              HTTP方法:GET
+//              串接的控制器&方法:ProductController@index
+
+//product.show URL:Products/{Product}
+//              HTTP方法:GET
+//              串接的控制器&方法:ProductController@show
+
+//product.create URL:Products/create
+//              HTTP方法:GET
+//              串接的控制器&方法:ProductController@create
+
+//product.store URL:/Products
+//              HTTP方法:POST
+//              串接的控制器&方法:ProductController@store
+
+//product.edit URL:Products/{Product}/edit
+//              HTTP方法:get
+//              串接的控制器&方法:ProductController@edit
+
+//product.update URL:Products/{Product}
+//              HTTP方法:PATCH
+//              串接的控制器&方法:ProductController@update
+
+//product.destroy URL:Products/{Product}
+//              HTTP方法:DELETE
+//              串接的控制器&方法:ProductController@destroy
+
+
