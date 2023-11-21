0dfd4c5926e1849d8b0439a960cd496169787c98
劉秉樺 <alen920617@gmail.com>
2023-11-21 22:48:58 +0800

    說明Products各個路由應該有的作用

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,53 @@ Route::resource('Products', ProductController::class);
 //              串接的控制器&方法:ProductController@destroy
 
 
+
+//上述七個Products路由應該有的作用
+//product.index    顯示資源清單。
+
+//product.show     顯示指定資源。
+
+//product.create   顯示用於建立新資源的表單。
+
+//product.store    將新建立的資源儲存在儲存庫中。
+
+//product.edit     顯示用於編輯指定資源的表單。
+
+//product.update   更新儲存中的指定資源。
+
+//product.destroy  顯示資源清單。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
